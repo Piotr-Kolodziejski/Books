61ade9f63cad28b5a8a4780e4316c7336f644381
Piotr <kolodziejski.p95@gmail.com>
2020-08-07 21:25:57 +0200

    add handling of POST request

--- Books/dbpost/views.py
+++ Books/dbpost/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 import json
 import requests
 from dbpost.models import Authors, Book
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
 
@@ -9,25 +10,27 @@ response = requests.get(
     'https://www.googleapis.com/books/v1/volumes?q=war')
 
 data = json.loads(response.text) 
-
-def post_to_database(request):
-    for i in data["items"]:
-        volumeInfo = i["volumeInfo"]
-        title = volumeInfo["title"]
-        authors = volumeInfo["authors"]
-        book_publish_date = volumeInfo["publishedDate"]
-        categories = volumeInfo.get("categories")
-        average_rating = volumeInfo.get("averageRating")
-        ratings_count = volumeInfo.get("ratingsCount")
-        image_links = volumeInfo["imageLinks"]
-        thumbnail = image_links["thumbnail"]
-        authors = Authors.objects.create(name = authors)
-        Book.objects.update_or_create(title = title, book_publish_date = book_publish_date, 
-                                    defaults = {'authors': authors, 'categories': categories,
-                                                'average_rating': average_rating, 'ratings_count': ratings_count,
-                                                'thumbnail': thumbnail})
+def post_to_database(request):        
+    if request.method == "POST":
+        body = json.loads(request.body)
+        if body == {"q": "war"}:
+            for i in data["items"]:
+                volumeInfo = i["volumeInfo"]
+                title = volumeInfo["title"]
+                authors = volumeInfo["authors"]
+                book_publish_date = volumeInfo["publishedDate"]
+                categories = volumeInfo.get("categories")
+                average_rating = volumeInfo.get("averageRating")
+                ratings_count = volumeInfo.get("ratingsCount")
+                image_links = volumeInfo["imageLinks"]
+                thumbnail = image_links["thumbnail"]
+                authors = Authors.objects.create(name = authors)
+                Book.objects.update_or_create(title = title, book_publish_date = book_publish_date, 
+                                            defaults = {'authors': authors, 'categories': categories,
+                                                        'average_rating': average_rating, 'ratings_count': ratings_count,
+                                                        'thumbnail': thumbnail})
+        return HttpResponse("Database updated!")
         
-
     return HttpResponse("hello")
     
 
