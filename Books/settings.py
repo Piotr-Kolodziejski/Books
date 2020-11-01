dd247ac0b6d15d5d52c254a58befc660cecc49b2
Piotr Kolodziejski <kolodziejski.p95@gmail.com>
2020-11-01 17:20:37 +0100

    change settings

--- Books/settings.py
+++ Books/settings.py
@@ -85,8 +85,7 @@ DATABASES = {
 }
 
 import dj_database_url
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Password validation
