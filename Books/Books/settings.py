af6111171258f23dc2737c282bbd4fd48799eb9b
Piotr Kolodziejski <kolodziejski.p95@gmail.com>
2020-11-01 14:38:27 +0100

    add changes to settings

--- Books/Books/settings.py
+++ Books/Books/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('KEY', 'default_value')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['booksort.herokuapp.com', '127.0.0.1']
 
